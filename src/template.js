9c24fa622f4bca9175946421ce07c8b36fe28597
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-11-23 21:01:18 -0500

    fixed last bugs final copy ready

--- src/template.js
+++ src/template.js
@@ -8,7 +8,7 @@ const generateHtml = team => {
 <div class="card employee-card">
   <div class="card-header bg-primary text-white">
     <h2 class="card-title">${manager.getName()}</h2>
-    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
   </div>
   <div class="card-body">
     <ul class="list-group">
@@ -26,7 +26,7 @@ const generateHtml = team => {
       <div class="card employee-card">
   <div class="card-header bg-primary text-white">
     <h2 class="card-title">${engineer.getName()}</h2>
-    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h3>
   </div>
   <div class="card-body">
     <ul class="list-group">
@@ -45,7 +45,7 @@ const generateHtml = team => {
       <div class="card employee-card">
   <div class="card-header bg-primary text-white">
     <h2 class="card-title">${intern.getName()}</h2>
-    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${intern.getRole()}</h3>
+    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h3>
   </div>
   <div class="card-body">
     <ul class="list-group">
@@ -98,6 +98,8 @@ const generateHtml = team => {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <link rel="stylesheet" href="./style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
+      integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <script
         type="text/javascript"
         src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"
